19deadf1f762186a34aa6c25c816bdb2206b31e8
Jonathan Gray <jsg@jsg.id.au>
2016-09-01 11:52:58 +0100

    Disable the code that allocates W|X memory on OpenBSD
    OpenBSD now has strict W^X enforcement.  Processes that violate
    the policy get killed by the kernel.  Don't attempt to use
    executable memory on OpenBSD to avoid this.
    
    Patch from Mark Kettenis.

--- src/mesa/main/execmem.c
+++ src/mesa/main/execmem.c
@@ -1,4 +1,5 @@
-/**************************************************************************
+/*
+ * Mesa 3-D graphics library
  *
  * Copyright (C) 1999-2005  Brian Paul   All Rights Reserved.
  *
@@ -19,47 +20,56 @@
  * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
- *
- **************************************************************************/
+ */
 
 
 /**
- * \file exemem.c
+ * \file execmem.c
  * Functions for allocating executable memory.
  *
  * \author Keith Whitwell
  */
 
 
-#include "pipe/p_compiler.h"
-#include "util/u_debug.h"
-#include "os/os_thread.h"
-#include "util/u_memory.h"
+#include <stdio.h>
+#include "main/glheader.h"
+#include "execmem.h"
+#include "c11/threads.h"
 
-#include "rtasm_execmem.h"
+
+#if defined(__OpenBSD__)
+
+void *
+_mesa_exec_malloc(GLuint size)
+{
+   return NULL;
+}
+
+void
+_mesa_exec_free(void *addr)
+{
+}
+
+#elif defined(__linux__) || defined(__NetBSD__) || defined(__sun) || defined(__HAIKU__)
+
+/*
+ * Allocate a large block of memory which can hold code then dole it out
+ * in pieces by means of the generic memory manager code.
+*/
+
+#include <unistd.h>
+#include <sys/mman.h>
+#include "util/u_mm.h"
+
+#ifdef MESA_SELINUX
+#include <selinux/selinux.h>
+#endif
+
 
 #ifndef MAP_ANONYMOUS
 #define MAP_ANONYMOUS MAP_ANON
 #endif
 
-#if defined(PIPE_OS_WINDOWS)
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN 1
-#endif
-#include <windows.h>
-#endif
-
-#if defined(PIPE_OS_UNIX)
-
-
-/*
- * Allocate a large block of memory which can hold code then dole it out
- * in pieces by means of the generic memory manager code.
- */
-
-#include <unistd.h>
-#include <sys/mman.h>
-#include "util/u_mm.h"
 
 #define EXEC_HEAP_SIZE (10*1024*1024)
 
@@ -69,35 +79,30 @@ static struct mem_block *exec_heap = NULL;
 static unsigned char *exec_mem = NULL;
 
 
-#ifdef __OpenBSD__
-
 static int
 init_heap(void)
 {
-   return 0;
-}
+#ifdef MESA_SELINUX
+   if (is_selinux_enabled()) {
+      if (!security_get_boolean_active("allow_execmem") ||
+	  !security_get_boolean_pending("allow_execmem"))
+         return 0;
+   }
+#endif
 
-#else
-
-static int
-init_heap(void)
-{
    if (!exec_heap)
       exec_heap = u_mmInit( 0, EXEC_HEAP_SIZE );
-   
+
    if (!exec_mem)
-      exec_mem = (unsigned char *) mmap(NULL, EXEC_HEAP_SIZE,
-					PROT_EXEC | PROT_READ | PROT_WRITE, 
-					MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+      exec_mem = mmap(NULL, EXEC_HEAP_SIZE, PROT_EXEC | PROT_READ | PROT_WRITE,
+                      MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
    return (exec_mem != MAP_FAILED);
 }
 
-#endif
-
 
 void *
-rtasm_exec_malloc(size_t size)
+_mesa_exec_malloc(unsigned size)
 {
    struct mem_block *block = NULL;
    void *addr = NULL;
@@ -108,30 +113,30 @@ rtasm_exec_malloc(size_t size)
       goto bail;
 
    if (exec_heap) {
-      size = (size + 31) & ~31;  /* next multiple of 32 bytes */
-      block = u_mmAllocMem( exec_heap, size, 5, 0 ); /* 5 -> 32-byte alignment */
+      size = (size + 31) & ~31;
+      block = u_mmAllocMem(exec_heap, size, 5, 0);
    }
 
    if (block)
       addr = exec_mem + block->ofs;
-   else 
-      debug_printf("rtasm_exec_malloc failed\n");
+   else
+      printf("_mesa_exec_malloc failed\n");
 
 bail:
    mtx_unlock(&exec_mutex);
-   
+
    return addr;
 }
 
- 
-void 
-rtasm_exec_free(void *addr)
+
+void
+_mesa_exec_free(void *addr)
 {
    mtx_lock(&exec_mutex);
 
    if (exec_heap) {
       struct mem_block *block = u_mmFindBlock(exec_heap, (unsigned char *)addr - exec_mem);
-   
+
       if (block)
 	 u_mmFreeMem(block);
    }
@@ -140,45 +145,23 @@ rtasm_exec_free(void *addr)
 }
 
 
-#elif defined(PIPE_OS_WINDOWS)
-
-
-/*
- * Avoid Data Execution Prevention.
- */
-
-void *
-rtasm_exec_malloc(size_t size)
-{
-   return VirtualAlloc(NULL, size, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-}
-
-
-void
-rtasm_exec_free(void *addr)
-{
-   VirtualFree(addr, 0, MEM_RELEASE);
-}
-
-
 #else
-
 
 /*
  * Just use regular memory.
  */
 
 void *
-rtasm_exec_malloc(size_t size)
+_mesa_exec_malloc(unsigned size)
 {
-   return MALLOC( size );
+   return malloc( size );
 }
 
- 
-void 
-rtasm_exec_free(void *addr)
+
+void
+_mesa_exec_free(void *addr)
 {
-   FREE(addr);
+   free(addr);
 }
 
 
