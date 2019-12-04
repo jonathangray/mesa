9e0f7d7d11e91ef53c7a75a3e15b1dc06efc400c
Jonathan Gray <jsg@jsg.id.au>
2019-12-04 16:34:13 +1100

    loader: check for setgid as well as setuid
    revision 1.8
    date: 2019/12/04 02:26:36;  author: deraadt;  state: Exp;  lines: +2 -2;  commitid: w6WeeZZ0y0WnF7VD;
    Constrain honouring of path-related environment variables based upon
    issetugid(), not just the sloppy uid != euid test. gid != egid cases
    can occur also.
    Part of 6.6/009_mesaxlock.patch.sig and 6.5/020_mesaxlock.patch.sig
    From Qualys, ok

--- src/util/u_debug.h
+++ src/util/u_debug.h
@@ -405,7 +405,7 @@ __normal_user(void)
 #if defined(_WIN32)
    return true;
 #else
-   return geteuid() == getuid() && getegid() == getgid();
+   return issetugid() == 0 && geteuid() == getuid() && getegid() == getgid();
 #endif
 }
 
