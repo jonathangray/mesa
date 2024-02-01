alexvorxx/zink-xlib-termux

commit 8b209a62006ad6fd4593bb4f528ce8aee23fc038

util: Provide a secure_getenv fallback for platforms without it
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27381>

src/util/u_debug.h
@@ -39,6 +39,7 @@
 #define U_DEBUG_H_
 
 #include <stdarg.h>
+#include <stdlib.h>
 #include <string.h>
 #if !defined(_WIN32)
 #include <sys/types.h>
@@ -402,6 +403,13 @@ __normal_user(void)
    return geteuid() == getuid() && getegid() == getgid();
 #endif
 }
+
+#ifndef HAVE_SECURE_GETENV
+static inline char *secure_getenv(const char *name)
+{
+   return getenv(name);
+}
+#endif
 
 #define DEBUG_GET_ONCE_BOOL_OPTION(sufix, name, dfault) \
 static bool \

meson.build
@@ -1378,6 +1378,7 @@ functions_to_detect = {
   'getrandom': '',
   'qsort_s': '',
   'posix_fallocate': '',
+  'secure_getenv': '',
 }
 
 foreach f, prefix: functions_to_detect

jonathangray/mesa

commit fd4aaf99cbd55a7dc5c9ee490bd929162cc0e242

util: use issetugid(2) in secure_getenv()
Fixes: 8b209a62006 ("util: Provide a secure_getenv fallback for platforms without it")

src/util/u_debug.h
@@ -407,6 +407,8 @@ __normal_user(void)
 #ifndef HAVE_SECURE_GETENV
 static inline char *secure_getenv(const char *name)
 {
+   if (issetugid())
+      return NULL;
    return getenv(name);
 }
 #endif