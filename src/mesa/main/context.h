07dd2e4a71cef0a55aa8f5277f8045d5449da096
Jonathan Gray <jsg@jsg.id.au>
2025-10-28 15:46:50 +1100

    Revert "glapi: remove the option to set the nop handler"
    This reverts commit 06a720e1eae6e0bb2be982c578f842068944e940.
    
    this breaks shared glapi w/o initial-exec tls

--- src/mesa/main/context.h
+++ src/mesa/main/context.h
@@ -110,9 +110,6 @@ _mesa_share_state(struct gl_context *ctx, struct gl_context *ctxToShare);
 extern struct gl_context *
 _mesa_get_current_context(void);
 
-extern void
-_mesa_noop_entrypoint(const char *name);
-
 /*@}*/
 
 extern void
